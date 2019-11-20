bd081807bcc195bd9b2f36f3b11ddb8101b44a2f
Ilusha <iliafeldgun@outlook.com>
2019-11-20 14:40:56 +0200

    Added searchbox

--- swfavesfe/src/Characters.tsx
+++ swfavesfe/src/Characters.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Characters.css';
-import { getAllPeople } from './Access'
+import { listenerCount } from 'cluster';
 //import { isTemplateElement } from '@babel/types';
 
 interface ICharacterProps {
@@ -19,49 +19,84 @@ class CharacterItem extends React.PureComponent<ICharacterProps, ICharacterState
 
     render() {
         return (
-            <li key={this.props.name} className="character">
+            <div className="character">
                 <h1 className="character-name">
                 {this.props.name}
                 </h1>
                 <input className="character-checkbox" type="checkbox" value={this.props.name}/>
-            </li>
+            </div>
         )
     }
 }
 
+interface ICharacterListProps {
+    listSource: Promise<string[]>,
+}
 interface ICharacterListState {
     list: string[],
+    filteredList: string[],
 }
-
-class CharacterList extends React.PureComponent<{},ICharacterListState>{
-    constructor (props: Readonly<{}>)
+class CharacterList extends React.PureComponent<ICharacterListProps,ICharacterListState>{
+    constructor (props: ICharacterListProps)
     {
         super (props);
         this.state = {
-            list: []
+            list: [],
+            filteredList: [],
         }
+
+        this.handleListChange = this.handleListChange.bind(this)
     }
 
     componentDidMount() {
-        getAllPeople().then((result) => 
-        {
-            this.setState({list: result})
+        this.props.listSource.then((result) => this.setState({list: result , filteredList: result,}))
+            
+        
+    }
+    handleListChange(event: React.ChangeEvent<HTMLInputElement>){
+        let newList = characterFilter(this.state.list.slice(), event.target.value)
+
+        this.setState({
+            filteredList: newList,
         })
     }
     
     render() {
-        const characterList = this.state.list.map(
+        const characterList = this.state.filteredList.map(
             item => (
                 <CharacterItem name={item}/>
             )
         )
-
-        console.log(this.state.list)
         return (
-            <ol>
-                {characterList}
-            </ol>
+            <div id="characterlist">
+                <SearchBox onChange={this.handleListChange}/>
+                <div id="characters">
+                    {characterList}
+                </div>
+            </div>
         )
     }
+}
+
+interface ISearchBoxProps {
+    onChange: (ev: React.ChangeEvent<HTMLInputElement>) => void,
+}
+class SearchBox extends React.PureComponent<ISearchBoxProps,{}>{
+    
+    render() {
+        return (
+        <input className="search-box" type="text" 
+        onChange={this.props.onChange}></input>
+        )
+    }
+}
+
+function characterFilter(list: string[], filter :string)
+{
+    let newlist = list.filter(element => {
+        return element.toLowerCase().indexOf(filter) != -1;
+    })
+
+    return newlist;
 }
 export default CharacterList;
--- swfavesfe/src/index.tsx
+++ swfavesfe/src/index.tsx
@@ -4,8 +4,9 @@ import ReactDOM from 'react-dom';
 import CharacterList from './Characters';
 //import { restElement } from '@babel/types';
 //import * as serviceWorker from './serviceWorker';
+import { getAllPeople } from './Access'
 
-ReactDOM.render(<CharacterList/> , document.getElementById('root'))
+ReactDOM.render(<CharacterList listSource={getAllPeople()}/> , document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
