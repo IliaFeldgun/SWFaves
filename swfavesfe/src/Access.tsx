1635f2cf06d06ebf5546cd5182b550f62d115313
Ilusha <iliafeldgun@outlook.com>
2019-11-20 17:25:12 +0200

    Added functions for second page

--- swfavesfe/src/Access.tsx
+++ swfavesfe/src/Access.tsx
@@ -5,5 +5,23 @@ export async function getAllPeople()
     let peopleResult = await fetch(peopleURL);
         
     return await peopleResult.json();
-        
+}
+
+async function postFavorites(list: string[], userID: number)
+{
+
+}
+
+async function moveToRecommendedMoviesPage(userID: number)
+{
+
+}
+
+export async function getRecommendedMovies(list: string[])
+{
+    let userID = 0
+    // userID = document.cookie.......
+    await postFavorites(list,userID);
+
+    moveToRecommendedMoviesPage(userID);
 }
