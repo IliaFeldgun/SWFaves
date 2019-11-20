c999ac471fd966b0200b485fd6f41a8536f55a09
Ilusha <iliafeldgun@outlook.com>
2019-11-20 17:24:20 +0200

    Changed root component

--- swfavesfe/src/index.tsx
+++ swfavesfe/src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
+import './General.css';
 import ReactDOM from 'react-dom';
 //import './index.css';
-import CharacterList from './Characters';
+import FavoriteCharacters from './Characters';
 //import { restElement } from '@babel/types';
 //import * as serviceWorker from './serviceWorker';
-import { getAllPeople } from './Access'
 
-ReactDOM.render(<CharacterList listSource={getAllPeople()}/> , document.getElementById('root'))
+ReactDOM.render(<FavoriteCharacters/> , document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
