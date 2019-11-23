78c9bf898fe89e85897582f3f8f81ed7f0a7a4ee
Ilusha <iliafeldgun@outlook.com>
2019-11-23 15:03:52 +0200

    Cleaned up project

--- swfavesfe/src/index.tsx
+++ swfavesfe/src/index.tsx
@@ -1,18 +1,7 @@
 import React from 'react';
 import './General.css';
 import ReactDOM from 'react-dom';
-//import './index.css';
-import FavoriteCharacters from './Characters';
-import App from './nav';
+import App from './routing';
 //import { restElement } from '@babel/types';
-//import * as serviceWorker from './serviceWorker';
-import { Router} from 'react-router';
 
-//ReactDOM.render(<FavoriteCharacters/> , document.getElementById('root'))
 ReactDOM.render(<App/> , document.getElementById('root'))
-
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-//serviceWorker.unregister();
