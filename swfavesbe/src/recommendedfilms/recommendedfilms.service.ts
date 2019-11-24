a5d0bb27c08b7bd4ecb025eb1560bc8d5f778f68
Ilusha <iliafeldgun@outlook.com>
2019-11-24 18:39:00 +0200

    Fake ID bug squashed

--- swfavesbe/src/recommendedfilms/recommendedfilms.service.ts
+++ swfavesbe/src/recommendedfilms/recommendedfilms.service.ts
@@ -12,37 +12,43 @@ export class RecommendedFilmsService {
         let allCharacters = this.characterService.getAllCharacters();
         let allFilms = await this.filmsService.getAllFilms();
 
-        let userCharacters = getCharactersFromName(await allCharacters, (await userCharactersNames).characters);
+        if (await userCharactersNames != null)
+        {
+            let userCharacters = getCharactersFromName(await allCharacters, (await userCharactersNames).characters);
 
-        allFilms.forEach((film : IFilmSuggestion) => {
-            userCharacters.forEach((userCharacter) => {
-                
-                // Sets up a score property
-                if (film.score == null || film.score == undefined)
-                {
-                    film.score = 0
-                }
+            allFilms.forEach((film : IFilmSuggestion) => {
+                userCharacters.forEach((userCharacter) => {
+                    
+                    // Sets up a score property
+                    if (film.score == null || film.score == undefined)
+                    {
+                        film.score = 0
+                    }
 
-                // Sets up favorite character property
-                if (film.favoriteCharacters == null || film.favoriteCharacters == undefined)
-                {
-                    film.favoriteCharacters = []
-                }
+                    // Sets up favorite character property
+                    if (film.favoriteCharacters == null || film.favoriteCharacters == undefined)
+                    {
+                        film.favoriteCharacters = []
+                    }
 
-                // Checks if film has current character
-                if (film.characters.indexOf(userCharacter.url) != -1)
-                {
-                    film.score++;
-                    film.favoriteCharacters.push(userCharacter.name);
-                }
+                    // Checks if film has current character
+                    if (film.characters.indexOf(userCharacter.url) != -1)
+                    {
+                        film.score++;
+                        film.favoriteCharacters.push(userCharacter.name);
+                    }
+                })
+            });
+            
+            allFilms.sort((filmA, filmB) => {
+                return filmB.score - filmA.score 
             })
-        });
+            
+            return allFilms;
+        }
+        else
+            return [];
         
-        allFilms.sort((filmA, filmB) => {
-            return filmB.score - filmA.score 
-        })
-
-        return allFilms;
 
         function getCharactersFromName(allCharacters: ISWAPICharacter[], userCharacters : string[]) : ISWAPICharacter[]
         {
