51a46657fdf6abbd1a4b3f751fbfc94622a8079f
Ilusha <iliafeldgun@outlook.com>
2019-11-24 16:33:50 +0200

    removed unnecessary schema

--- swfavesbe/src/schemas/favoritecharacters.schema.ts
+++ swfavesbe/src/schemas/favoritecharacters.schema.ts
@@ -1,12 +1,6 @@
-
 import * as mongoose from 'mongoose';
 
 export const UserCharacterSchema = new mongoose.Schema({
   _id: Number,
   characters: [String],
 });
-
-export const UserFilmsSchema = new mongoose.Schema({
-    _id: Number,
-    films: Array,
-  });
