2579eb0b1a5d2929231e01755c4801c545b620f5
Ilusha <iliafeldgun@outlook.com>
2019-11-24 16:37:40 +0200

    removed unnecessary modules

--- swfavesbe/src/app.module.ts
+++ swfavesbe/src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module, MiddlewareConsumer } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CharactersController } from './characters/characters.controller';
 import { CharactersService } from './characters/characters.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { UserCharacterSchema, UserFilmsSchema} from './schemas/favoritecharacters.schema';
+import { UserCharacterSchema} from './schemas/favoritecharacters.schema';
 import { FilmsService } from './films/films.service';
 import { CookieMiddleware } from './cookie.middleware';
 import { ServeStaticModule } from '@nestjs/serve-static';
@@ -16,10 +14,9 @@ require('dotenv').config()
 @Module({
   imports: [ServeStaticModule.forRoot({rootPath: join(__dirname, "..", 'client'),}),
             MongooseModule.forRoot(process.env.MONGO), 
-            MongooseModule.forFeature([{ name: 'UserFilms', schema: UserFilmsSchema }]), 
             MongooseModule.forFeature([{ name: 'UserCharacters', schema: UserCharacterSchema }])],
-  controllers: [AppController, CharactersController, RecommendedfilmsController],
-  providers: [AppService, CharactersService, FilmsService, RecommendedFilmsService],
+  controllers: [CharactersController, RecommendedfilmsController],
+  providers: [CharactersService, FilmsService, RecommendedFilmsService],
 })
 export class AppModule {
   configure(consumer: MiddlewareConsumer){
